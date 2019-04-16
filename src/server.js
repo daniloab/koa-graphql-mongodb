c1d08a1fedf9a74705e04e54150e509bd542cd9b
Danilo Assis <danilo.botaro@tribofox.com.br>
2019-04-16 09:31:53 -0300

    Add @koa/cors

--- src/server.js
+++ src/server.js
@@ -4,13 +4,14 @@ const Koa = require('koa')
 const mount = require('koa-mount');
 const graphqlHTTP = require('koa-graphql');
 const schema = require('./graphql/schema');
+const cors = require('@koa/cors')
 
 const initDB = require('./database/database')
 
 initDB()
 
 const app = new Koa()
-
+app.use(cors())
 app.listen(process.env.PORT || 9000)
 
 app.use(mount('/graphql', graphqlHTTP({
