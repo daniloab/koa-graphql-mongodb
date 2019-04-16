5292dbee2c93b92fa6cdb154559052f4ae13d74d
Danilo Assis <danilo.botaro@tribofox.com.br>
2019-04-16 09:02:29 -0300

    Configuring env variables

--- src/database/database.js
+++ src/database/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const initDB = () => {
     mongoose.connect(
-        'mongodb://danassis:daniloe3r4tkd@ds239936.mlab.com:39936/first-koa-graphql-mongodb',
+        process.env.MONGODB_URI,
         { useNewUrlParser: true }
     )
 
--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const Koa = require('koa')
 
 const mount = require('koa-mount');
